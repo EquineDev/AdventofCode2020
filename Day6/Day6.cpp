1d330f4b4b35447e47884ad300071e1ed9b9552d
Scott Tongue <Stongue87@gmail.com>
2020-12-07 11:50:42 -0500

    Finished day 6 solution

--- Day6/Day6.cpp
+++ Day6/Day6.cpp
@@ -1,69 +1,95 @@
 // Day6.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iostream>
 #include <vector>
-#include <unordered_set>
-#include <algorithm>
 
-std::vector<std::vector<std::string>> data;
+
+std::vector<std::vector<std::string>> Data;
 void GetInput(std::string FileData)
 {
 
-    std::ifstream file(FileData);
-    std::string line;
-    std::vector<std::string> group;
-    while (std::getline(file, line))
-    {
-        if (line.length() == 0)
-        {
-            group.push_back(line);
-            data.clear();
-        }
-        else
-            group.push_back(line);
-       
-       
-    }
-    data.push_back(group);
-    file.close();
+	std::ifstream file(FileData);
+
+	std::string line;
+	std::vector<std::string> GroupQuestions;
+	while (std::getline(file, line))
+	{
+		if (line.length() == 0)
+		{
+			Data.push_back(GroupQuestions);
+			GroupQuestions.clear();
+		}
+		else
+			GroupQuestions.push_back(line);
+
+	}
+	Data.push_back(GroupQuestions);
+
+	file.close();
+
 }
 
-int AnyQuestionSum() 
+
+int SumPart1()
 {
-    int count = 0;
-    std::unordered_set<char> answers;
+	int count = 0;
+	for (auto group : Data)
+	{
+	
+		bool questions[26] = {};
+		for (auto person : group)
+		{
+			for (auto ch : person)
+			{
+				questions[ch - 'a'] = true;
+			}
+		}
 
-    for (auto group : data) 
-    {
-        for (auto line : group) 
-        {
+		for (auto question : questions)
+		{
+			if (question)
+				count++;
 
-            answers.insert(line.begin(), line.end());
-        }
-        count += answers.size();
-        answers.clear();
-    }
-    return count; 
+		}
+	}
+	return count;
+}
+
+int SumPart2()
+{
+	int count = 0;
+	for (auto group : Data)
+	{
+		
+		int questions[26] = {};
+		for (auto person : group)
+		{
+			for (auto ch : person)
+			{
+				questions[ch - 'a']++;
+			}
+		}
+
+		for (auto question : questions)
+		{
+			if (question == group.size())
+				count++;
+		}
+	}
+	return count;
 }
 
 int main()
 {
-    GetInput("Day6Data.txt");
-    std::cout << "Number of anwser is : " << AnyQuestionSum() << std::endl;
-    system("Pause");
+	
+	GetInput("Day6Data.txt");
+	std:: cout <<"Sum of questions part 1: " <<SumPart1() << std::endl;
+	std::cout << "Sum of questions part 2: " << SumPart2() << std::endl;
+	system("Pause");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
